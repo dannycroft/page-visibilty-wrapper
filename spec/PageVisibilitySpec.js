44b7287b8aff5451ccbb2fdb4c95545d9cb8c71f
Danny Croft <daniel.croft@livenation.co.uk>
2013-06-05 14:17:28 +0100

    Initial commit

---
+++ spec/PageVisibilitySpec.js
@@ -0,0 +1,23 @@
+describe("Page Visibility", function() {
+
+    it("should verify API support", function() {
+
+    });
+
+    it("should confirm the page is visible", function() {
+
+    });
+
+    it("should confirm the page is hidden", function() {
+
+    });
+
+    it("should get the page state", function() {
+
+    });
+
+    it("should add a callback to the visibility change event", function() {
+
+    });
+
+});
